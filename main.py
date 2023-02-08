ae46105a3984c2e7636d86fe8a112cab89ee92e6
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-07 16:03:22 -0800

    02-2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     ##################################################
     # Comlete your code here
+    C=int(input("Enter Celsius: "))
+    F=9/5*C+32
+    print("Fahrenheit is: {0:.2f}".format(F))
     ##################################################
     pass
 
